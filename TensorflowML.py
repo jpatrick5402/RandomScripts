c2141d1ad8eb8b6eb5c9c5b0b95b781441d6d186
jpatrick5402 <jpatrick5402@gmail.com>
2022-11-15 22:29:11 -0500

    More files with ML stuff

---
+++ TensorflowML.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+
+"""
+Tensorflow usually follows this pattern
+1. Import Dataset
+2. Extend dataset with additional columns to describe the data
+3. Select the type of model
+4. Training
+5. Evaluate accuracy of the model
+6. Predict results using the model
+"""
+
